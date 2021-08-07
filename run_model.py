538f368399b4de52247db500dabfaddc2f72326c
Filippo Uslenghi <us98@hotmail.it>
2021-08-07 12:14:48 +0200

    update

--- run_model.py
+++ run_model.py
@@ -7,6 +7,7 @@ file = model + '.py'
 emotions = ['anger', 'disgust', 'fear',
             'happiness', 'sadness', 'surprise', 'valence']
 poses = ['tilted', 'frontal', 'none']
+au_features = ['au_intensities', 'au_activations', 'au_intensities_activations']
 
 for emotion in emotions:
 
@@ -14,6 +15,12 @@ for emotion in emotions:
         os.system(' '.join(['python', file, emotion, features]))
         # print(' '.join(['python', file, emotion, features]))
     else:
-        for pose in poses:
-            os.system(' '.join(['python', file, emotion, pose, features]))
-            # print(' '.join([file, emotion, pose, features]))
+        if features == 'au':
+            for pose in poses:
+                for au_feature in au_features:
+                    os.system(' '.join(['python', file, emotion, pose, au_feature]))
+                    # print(' '.join([file, emotion, pose, au_feature]))    
+        else:
+            for pose in poses:
+                os.system(' '.join(['python', file, emotion, pose, features]))
+                # print(' '.join([file, emotion, pose, features]))
