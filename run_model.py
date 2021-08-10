b3cdcb96970090de264ec56de9b967162a0f528d
Filippo Uslenghi <us98@hotmail.it>
2021-08-10 18:47:06 +0200

    update

--- run_model.py
+++ run_model.py
@@ -21,7 +21,7 @@ for features in features_list:
 
         elif script == 'kernel_SVM.py':
             if features == 'delaunay': continue
-            os.system(' '.join(['python', script, emotion, features]))
+            os.system(' '.join(['python', script, emotion, pose, features]))
 
         else:
             for pose in poses:
