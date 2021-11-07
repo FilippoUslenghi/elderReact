c25f842eb74e932669366167f7d5268c72fdf653
Filippo Uslenghi <us98@hotmail.it>
2021-11-07 22:19:34 +0100

    update

--- features_extraction.sh
+++ features_extraction.sh
@@ -8,11 +8,11 @@ echo "Creando le features di Delaunay..."
 python3 delaunay.py
 
 echo "##############################################################################################
-Per poter eseguire 'my_demo_FSANET_sdd.py' leggi i requirements scritti sul github ufficiale di FSA-Net
+Per poter eseguire 'my_demo_FSANET_sdd.py' leggi i requisiti scritti sulla pagina GitHub di FSA-Net
 ##############################################################################################"
 
 cd ../FSA-Net/demo/
-echo "Estraendo le features sull'orientazione del volto con FSA-Net... (può richiedere qualche ora)"
+echo "Estraendo le features sull'orientazione del volto con FSA-Net... (può richiedere più ore)"
 
 python3 my_demo_FSANET_sdd.py
 
