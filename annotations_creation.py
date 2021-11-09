134b1d815debec351f02715b10e437df1ba5a90f
Filippo Uslenghi <us98@hotmail.it>
2021-11-09 12:47:49 +0100

    update

--- annotations_creation.py
+++ annotations_creation.py
@@ -7,18 +7,19 @@ import os
 groups = ['train', 'dev', 'test']
 for group in groups:
 
-    old_annotations_path = os.path.join('dataset', 'ElderReact-master', 'Annotations', f'{group}_labels.txt')
-    new_annotations_path = os.path.join('my_dataset', 'Annotations', f'{group}_labels.txt')
+    annotation_file = f'{group}_labels.txt'
+    old_annotations_path = os.path.join('dataset', 'ElderReact-master', 'Annotations')
+    new_annotations_path = os.path.join('my_dataset', 'Annotations')
     os.makedirs(new_annotations_path, exist_ok=True)
     line_list = []
-    with open(old_annotations_path) as f:
+    with open(os.path.join(old_annotations_path, annotation_file)) as f:
         for line in f.readlines():
             video_name = line.split(sep=' ')[0].replace('.mp4', '.csv')
             if video_name in os.listdir(os.path.join('my_dataset', 'Features', group, 'interpolated_AU_')):
                 line_list.append(line)
                 
         try:
-            with open(new_annotations_path, 'x') as new_f:
+            with open(os.path.join(new_annotations_path, annotation_file), 'x') as new_f:
                 new_f.write(''.join([line for line in line_list]))
         except FileExistsError:
             pass
