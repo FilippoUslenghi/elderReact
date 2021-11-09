06e38dcd83e7510618b416ff7d3e4af6e8d74600
Filippo Uslenghi <us98@hotmail.it>
2021-11-09 09:12:54 +0100

    update

--- features_extraction_2.sh
+++ features_extraction_2.sh
@@ -2,6 +2,6 @@ echo "Ultimando il dataset..."
 python3 pose_dataset_creation.py
 python3 annotations_creations.py
 
-echo "Il dataset è stato creato con successo."
+echo "Il dataset è stato creato con successo. Lo puoi trovare nella directory 'my_dataset'."
 echo "Per eseguire l'addestramento dei modelli di machine learning esegui lo script 'run_models.py'.
 L'apprendimento durerà circa 1 giorno."
