5e2cb3441f3d9528cf4f3efd293f841ca20e99ca
Filippo Uslenghi <us98@hotmail.it>
2021-11-09 09:36:03 +0100

    update

--- features_extraction_2.sh
+++ features_extraction_2.sh
@@ -5,3 +5,5 @@ python3 annotations_creations.py
 echo "Il dataset è stato creato con successo. Lo puoi trovare nella directory 'my_dataset'."
 echo "Per eseguire l'addestramento dei modelli di machine learning esegui lo script 'run_models.py'.
 L'apprendimento durerà circa 1 giorno."
+echo "Per creare un .csv con i risultati ottenuti dai modelli basta eseguire lo script 'results_dataframe_creator.py'
+presente nella directory 'results'"
